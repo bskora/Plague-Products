ba7afec23c9e9076b185519e49760ff239385713
Trenix <trenixdecease@live.com>
2022-05-17 15:27:42 -0400

    Added news section, fully functional using the database. Capitalized main categories on the website.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateField(auto_now=True)
+    # body = models.TextField()
+    body = RichTextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title + " | " + str(self.date)
--- main/views.py
+++ main/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from main.models import Post
 
 # Create your views here.
+class NewsView(ListView):
+    model = Post
+    template_name = "news.html"
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(NewsView, self).get_queryset(*args, **kwargs)
+        qs = qs.order_by("-id")
+        return qs
+
 def home(response):
     return render(response, "home.html", {})
+
+def news(response):
+    return render(response, "news.html", {})
 
 def code(response):
     return render(response, "codenet.html", {})
