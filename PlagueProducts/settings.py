d13226b5cf73254c30fe73a2f6387254c816fa33
Trenix <trenixdecease@live.com>
2022-05-19 23:26:21 -0400

    test for deployment

--- PlagueProducts/settings.py
+++ PlagueProducts/settings.py
@@ -112,8 +112,6 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'EST'
 
-SITE_ID = 1
-
 USE_I18N = True
 
 USE_TZ = True
@@ -123,10 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
