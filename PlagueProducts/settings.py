c7f121b2f833563ad7ac192e41cd289b3301af2f
Trenix <trenixdecease@live.com>
2022-05-20 11:07:33 -0400

    test for deployment

--- PlagueProducts/settings.py
+++ PlagueProducts/settings.py
@@ -14,6 +14,12 @@ from pathlib import Path
 import environ
 import os
 
+# Set the project base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
