7aa6fcd4fdcaf4a398a129e9b8c297c4c82195fa
Trenix <trenixdecease@live.com>
2022-05-19 22:31:35 -0400

    test for deployment

--- PlagueProducts/settings.py
+++ PlagueProducts/settings.py
@@ -14,8 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -111,6 +110,8 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'EST'
+
+SITE_ID = 1
 
 USE_I18N = True
 
