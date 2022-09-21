f9755bb4e5dce24bf2582c229608ac7a0a9cf5fe
Trenix <trenixdecease@live.com>
2022-09-20 22:47:47 -0400

    Potential completion of postgres database.

--- PlagueProducts/settings.py
+++ PlagueProducts/settings.py
@@ -135,7 +135,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
