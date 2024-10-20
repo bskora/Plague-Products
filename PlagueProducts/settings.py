6fd92daab96875de271a57344b2db6c9eb59d1ee
Bart <bskora@live.com>
2024-10-20 02:28:35 -0400

    Fixing layout for official deployment

--- PlagueProducts/settings.py
+++ PlagueProducts/settings.py
@@ -161,10 +161,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = BASE_DIR / "staticfiles"
